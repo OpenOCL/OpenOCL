a35e5b12aeda3b433041be5f53dca7aaddf7f4ac
Jonas Koenemann <jonas@Jonas-Koenemanns-MacBook.local>
2017-01-06 14:11:45 +0100

    added gitignore

--- StartupOC.m
+++ StartupOC.m
@@ -13,7 +13,6 @@ addpath(fullfile(startupDir,'CasadiLibrary','Test'))
 addpath(fullfile(startupDir,'Core'))
 addpath(fullfile(startupDir,'Core','Test'))
 addpath(fullfile(startupDir,'Core','Simultaneous'))
-addpath(fullfile(startupDir,'Core','UserInterface'))
 addpath(fullfile(startupDir,'Core','Experimental'))
 addpath(fullfile(startupDir,'Core','Integrator'))
 
