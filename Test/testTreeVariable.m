14212ed03c699cdc8bdadcb93df5e9d4b114eb42
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-01-16 11:00:55 +0100

    EOF

--- Test/testTreeVariable.m
+++ Test/testTreeVariable.m
@@ -98,7 +98,6 @@ assertEqual(v.x.R(2,3,4).value, A(2,3,4));
 assertEqual(v.x.R(:).value, A(:));
 assertEqual(v.x.R(:,:).value, A(:,:));
 
-
 assertSqueezeEqual(v.x.R(:,:,:).value, A(:,:,:) );
 
 assertEqual(v.x.R(end).value, A(end));
@@ -113,7 +112,6 @@ assertSqueezeEqual(v.x.R(1:2,[1,3],2:5).value, A(1:2,[1,3],2:5));
 assertSqueezeEqual(v.x.R(:,2,:).value, A(:,2,:));
 assertEqual(v.x.R(:,:,3).value, A(:,:,3));
 assertEqual(v.x.R(:,2).value, A(:,2));
-
 
 % set tests
 if ~isOctave()
