bb956b4c0b71dd83b827cd2733d1a0dde09600dd
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-04-09 10:15:05 +0200

    return u from simulation callback

--- Core/Integrator/Simulator.m
+++ Core/Integrator/Simulator.m
@@ -1,41 +1,41 @@
 classdef Simulator < handle
-  
+
   properties
     integrator
     system
     options
   end
-  
+
   methods (Static)
     function options = getOptions()
       options = struct;
     end
   end
-  
+
   methods
-    
+
     function self = Simulator(system,options)
-      
+
       if nargin==1
         self.options = Simulator.getOptions();
       else
         self.options = options;
       end
-      
+
       system.setup();
-      
+
       self.integrator = CasadiIntegrator(system);
       self.system = system;
       self.system.systemFun = CasadiFunction(self.system.systemFun);
     end
-    
+
     function controlsVec = getControlsVec(self,N)
         controlsVecStruct  = OclStructure();
         controlsVecStruct.addRepeated({'u'},{self.system.controlsStruct},N);
         controlsVec = Variable.create(controlsVecStruct,0);
         controlsVec = controlsVec.u;
     end
-    
+
     function states = getStates(self)
       states = Variable.create(self.system.statesStruct,0);
     end
@@ -43,12 +43,12 @@ classdef Simulator < handle
     function states = getParameters(self)
       states = Variable.create(self.system.parametersStruct,0);
     end
-    
+
     function [statesVec,algVarsVec,controlsVec] = simulate(self,initialStates,times,varargin)
       % [statesVec,algVarsVec,controlsVec] = simulate(self,initialState,times,parameters)
       % [statesVec,algVarsVec,controlsVec] = simulate(self,initialState,times,controlsVec,parameters)
       % [statesVec,algVarsVec,controlsVec] = simulate(self,initialState,times,controlsVec,parameters,callback)
-      
+
       N = length(times)-1;
       callback = true;
       times = Variable.Matrix(times);
@@ -63,57 +63,57 @@ classdef Simulator < handle
         parameters = varargin{2};
         callback = varargin{3};
       end
-      
+
       statesVecStruct = OclStructure();
       statesVecStruct.addRepeated({'x'},{self.system.statesStruct},N+1);
-      
+
       algVarsVecStruct = OclStructure();
       algVarsVecStruct.addRepeated({'z'},{self.system.algVarsStruct},N);
-      
+
       statesVec = Variable.create(statesVecStruct,0);
       statesVec = statesVec.x;
       algVarsVec = Variable.create(algVarsVecStruct,0);
       algVarsVec = algVarsVec.z;
-      
+
       nz = self.system.nz;
       nu = self.system.nu;
-      
+
       z = zeros(nz,1);
       x0 = Variable.getValueAsColumn(initialStates);
       p = Variable.getValueAsColumn(parameters);
       times = Variable.getValueAsColumn(times);
-      
+
       u0 = Variable.getValueAsColumn(controlsVec(:,:,1));
-      
+
       [x,z] = self.getConsistentIntitialCondition(x0,z,u0,p);
       statesVec(:,:,1).set(x);
-      
+
       % setup callback
       global testRun
       if callback && (isempty(testRun) || (testRun==false))
         self.system.simulationCallbackSetup;
       end
-      
+
       for k=1:N-1
-        
+
         t = times(k+1)-times(k);
         u = Variable.getValueAsColumn(controlsVec(:,:,k));
-        
+
         if callback && (isempty(testRun) || (testRun==false))
-          self.system.callSimulationCallback(x,z,u,times(k),times(k+1),p);
+          u = self.system.callSimulationCallback(x,z,u,times(k),times(k+1),p);
         end
-        
+
         [x,z] = self.integrator.evaluate(x,z,u,t,p);
-        
+
         statesVec(:,:,k+1).set(x);
         algVarsVec(:,:,k).set(z);
         controlsVec(:,:,k).set(u);
-      end  
+      end
       if callback && (isempty(testRun) || (testRun==false))
-        self.system.callSimulationCallback(x,z,u,times(k),times(k+1),p);
+        u = self.system.callSimulationCallback(x,z,u,times(k),times(k+1),p);
       end
     end
-    
+
     function [xOut,zOut] = getConsistentIntitialCondition(self,x,z,u,p)
 
       xSymb  = casadi.SX.sym('x',size(x));
@@ -136,12 +136,12 @@ classdef Simulator < handle
 
       sol = full(sol.x);
       nx = size(x,1);
-      
+
       xOut = sol(1:nx);
       zOut = sol(nx+1:end);
-      
+
     end
-    
+
   end
-  
+
 end
--- Core/OclSystem.m
+++ Core/OclSystem.m
@@ -1,141 +1,141 @@
 classdef OclSystem < handle
-  
+
   properties
     statesStruct
     algVarsStruct
     controlsStruct
     parametersStruct
-    
+
     ode
     alg
-    
+
     fh
-    
+
     bounds
-    
+
     thisInitialConditions
-    
+
     systemFun
     icFun
   end
-  
+
   properties (Access = private)
     odeVar
   end
 
   methods
-    
+
     function self = OclSystem(varargin)
       % OclSystem()
       % OclSystem(fhVarSetup,fhEquationSetup)
       % OclSystem(fhVarSetup,fhEquationSetup,fhInitialCondition)
-      
+
       defFhVars = @(varargin)self.setupVariables(varargin{:});
       defFhEq = @(varargin)self.setupEquations(varargin{:});
       defFhIC = @(varargin)self.initialConditions(varargin{:});
-      
+
       p = inputParser;
       p.addOptional('fhVars',defFhVars,@oclIsFunHandle);
       p.addOptional('fhEq',defFhEq,@oclIsFunHandle);
       p.addOptional('fhIC',defFhIC,@oclIsFunHandle);
       p.parse(varargin{:});
-      
+
       self.fh = struct;
       self.fh.vars = p.Results.fhVars;
       self.fh.eq = p.Results.fhEq;
       self.fh.ic = p.Results.fhIC;
-      
+
       self.statesStruct     = OclStructure();
       self.algVarsStruct    = OclStructure();
       self.controlsStruct   = OclStructure();
       self.parametersStruct = OclStructure();
-      
+
       self.bounds = struct;
       self.ode = struct;
     end
-    
+
     function r = nx(self)
       r = prod(self.statesStruct.size());
     end
-    
+
     function r = nz(self)
       r = prod(self.algVarsStruct.size());
     end
-    
+
     function r = nu(self)
       r = prod(self.controlsStruct.size());
     end
-    
+
     function r = np(self)
       r = prod(self.parametersStruct.size());
     end
-    
+
     function setup(self)
-      
+
       self.fh.vars(self);
-      
+
       sx = self.statesStruct.size();
       sz = self.algVarsStruct.size();
       su = self.controlsStruct.size();
       sp = self.parametersStruct.size();
-      
+
       fhEq = @(self,varargin)self.getEquations(varargin{:});
       self.systemFun = OclFunction(self, fhEq, {sx,sz,su,sp},2);
-      
+
       fhIC = @(self,varargin)self.getInitialConditions(varargin{:});
       self.icFun = OclFunction(self, fhIC, {sx,sp},1);
     end
-    
+
     function setupVariables(varargin)
       error('Not Implemented.');
     end
     function setupEquations(varargin)
       error('Not Implemented.');
     end
-    
+
     function initialConditions(~,~,~)
       % initialConditions(states,parameters)
     end
-    
+
     function simulationCallbackSetup(~)
       % simulationCallbackSetup()
     end
-    
+
     function simulationCallback(varargin)
       % simulationCallback(states,algVars,controls,timeBegin,timesEnd,parameters)
     end
-    
+
     function [ode,alg] = getEquations(self,states,algVars,controls,parameters)
       % evaluate the system equations for the assigned variables
-      
+
       % reset alg and ode
       self.alg = [];
       names = fieldnames(self.ode);
       for i=1:length(names)
         self.ode.(names{i}) = [];
       end
-      
+
       x = Variable.create(self.statesStruct,states);
       z = Variable.create(self.algVarsStruct,algVars);
       u = Variable.create(self.controlsStruct,controls);
       p = Variable.create(self.parametersStruct,parameters);
 
       self.fh.eq(self,x,z,u,p);
-     
+
       ode = struct2cell(self.ode);
       ode = vertcat(ode{:});
       alg = self.alg;
-      
+
       if length(alg) ~= self.nz
         oclException(['Number of algebraic equations does not match ',...
                       'number of algebraic variables.']);
-      end      
+      end
       if length(ode) ~= self.nx
         oclException(['Number of ode equations does not match ',...
                       'number of state variables.']);
       end
     end
-    
+
     function ic = getInitialConditions(self,states,parameters)
       icHandler = OclConstraint(self);
       x = Variable.create(self.statesStruct,states);
@@ -150,16 +150,16 @@ classdef OclSystem < handle
       % addState(id)
       % addState(id,s)
       % addState(id,s,lb=lb,lb=ub)
-      
+
       p = inputParser;
       p.addRequired('id', @ischar);
       p.addOptional('s', 1, @isnumeric);
       p.addParameter('lb', -inf, @isnumeric);
       p.addParameter('ub', inf, @isnumeric);
       p.parse(id,varargin{:});
-      
+
       id = p.Results.id;
-      
+
       self.ode.(id) = [];
       self.statesStruct.add(id, p.Results.s);
       self.bounds.(id).lower = p.Results.lb;
@@ -175,9 +175,9 @@ classdef OclSystem < handle
       p.addParameter('lb', -inf, @isnumeric);
       p.addParameter('ub', inf, @isnumeric);
       p.parse(id,varargin{:});
-      
+
       id = p.Results.id;
-      
+
       self.algVarsStruct.add(id, p.Results.s);
       self.bounds.(id).lower = p.Results.lb;
       self.bounds.(id).upper = p.Results.ub;
@@ -192,9 +192,9 @@ classdef OclSystem < handle
       p.addParameter('lb', -inf, @isnumeric);
       p.addParameter('ub', inf, @isnumeric);
       p.parse(id,varargin{:});
-      
+
       id = p.Results.id;
-      
+
       self.controlsStruct.add(id,p.Results.s);
       self.bounds.(id).lower = p.Results.lb;
       self.bounds.(id).upper = p.Results.ub;
@@ -208,9 +208,9 @@ classdef OclSystem < handle
       p.addOptional('s', 1, @isnumeric);
       p.addParameter('default', 0, @isnumeric);
       p.parse(id,varargin{:});
-      
+
       id = p.Results.id;
-      
+
       self.parametersStruct.add(id,p.Results.s);
       self.bounds.(id).lower = p.Results.default;
       self.bounds.(id).upper = p.Results.default;
@@ -225,21 +225,21 @@ classdef OclSystem < handle
       end
       self.ode.(id) = Variable.getValueAsColumn(eq);
     end
-    
+
     function setAlgEquation(self,eq)
       self.alg = [self.alg;Variable.getValueAsColumn(eq)];
     end
-    
+
     function setInitialCondition(self,eq)
-      self.thisInitialConditions = [self.thisInitialConditions; Variable.getValueAsColumn(eq)];      
+      self.thisInitialConditions = [self.thisInitialConditions; Variable.getValueAsColumn(eq)];
     end
-    
+
     function solutionCallback(self,times,solution)
       sN = size(solution.states);
       N = sN(3);
       parameters = solution.parameters;
       t = times.states;
-      
+
       for k=1:N-1
         states = solution.states(:,:,k+1);
         algVars = solution.integrator(:,:,k).algVars;
@@ -247,20 +247,19 @@ classdef OclSystem < handle
         self.simulationCallback(states,algVars,controls,t(:,:,k),t(:,:,k+1),parameters);
       end
     end
-    
-    function callSimulationCallback(self,states,algVars,controls,timesBegin,timesEnd,parameters)
+
+    function u = callSimulationCallback(self,states,algVars,controls,timesBegin,timesEnd,parameters)
       x = Variable.create(self.statesStruct,states);
       z = Variable.create(self.algVarsStruct,algVars);
       u = Variable.create(self.controlsStruct,controls);
       p = Variable.create(self.parametersStruct,parameters);
-      
+
       t0 = Variable.Matrix(timesBegin);
       t1 = Variable.Matrix(timesEnd);
-      
+
       self.simulationCallback(x,z,u,t0,t1,p);
-      
+      u = Variable.getValueAsColumn(u);
     end
-    
+
   end
 end
-
