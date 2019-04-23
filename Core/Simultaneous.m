d941460d602b8f9c3f264117054d3a9b90189647
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-04-23 15:13:39 +0200

    simultaneous w phases

--- Core/Simultaneous.m
+++ Core/Simultaneous.m
@@ -6,7 +6,9 @@ classdef Simultaneous < handle
     nlpFun
     varsStruct
     timesStruct
-    ocpHandler
+    
+    phaseHandler
+    
     integratorFun
     nv
     system
@@ -23,7 +25,7 @@ classdef Simultaneous < handle
     nu
     np
     nit
-    nq    % number of phases
+    numPhases
     
     igBoundsAll
     igBounds0
@@ -34,35 +36,28 @@ classdef Simultaneous < handle
   end
   
   methods
-    function self = Simultaneous(system,phaseHandler,integrator,N,options)
+    function self = Simultaneous(system,phaseHandler,options)
       
       self.system = system;
-      self.ocpHandler = phaseHandler;
-      self.N = N;
+      self.phaseHandler = phaseHandler;
       self.options = options;
+      
       self.nx = prod(system.statesStruct.size());
       self.ni = prod(integrator.varsStruct.size());
       self.nu = prod(system.controlsStruct.size());
       self.np = prod(system.parametersStruct.size());
-      self.nq = phaseHandler.nq;
+      self.numPhases = phaseHandler.numPhases;
       
       % N control interval which each have states, integrator vars,
       % controls, parameters, and timesteps.
       % Ends with a single state.
-      self.nv = (N+1)*self.nx + N*self.ni + N*self.nu + N*self.np + N;
+      self.nv = self.numPhases*(N*self.nx + N*self.ni + N*self.nu + N*self.np + N) + self.nx;
       
       self.nit = integrator.nt; % number of integrator timepoints
       
       self.integratorFun = integrator.integratorFun;
       
-      self.varsStruct = OclStructure();
-      self.varsStruct.addRepeated({'states','integrator','controls','parameters','h','T'}, ...
-                                  {system.statesStruct, ...
-                                   integrator.varsStruct, ...
-                                   system.controlsStruct, ...
-                                   system.parametersStruct, ...
-                                   OclMatrix([1,1])}, self.N);
-      self.varsStruct.add('states',system.statesStruct);
+      self.varsStruct = self.getVarsStruct(phaseHandler);
       
       self.timesStruct = OclStructure();
       self.timesStruct.addRepeated({'states','integrator','controls'},...
@@ -77,6 +72,36 @@ classdef Simultaneous < handle
       self.igBoundsF = struct;
       
       self.igParameters = struct;
+      
+    end
+    
+    
+    function varsStruct = getVarsStruct(~, phaseHandler)
+      
+      varsStruct = OclStructure();
+      phaseStruct = [];
+      
+      for k=1:phaseHandler.numPhases
+        
+        phase = phaseHandler.get(k);
+        
+        phaseStruct = OclStructure();
+        phaseStruct.addRepeated({'states','integrator','controls','parameters','h'}, ...
+                            {phase.statesStruct, ...
+                             phase.integrator.varsStruct, ...
+                             phase.controlsStruct, ...
+                             phase.parametersStruct, ...
+                             OclMatrix([1,1])}, phase.N);
+        phaseStruct.add('states', phase.statesStruct);
+
+        varsStruct.add('phase', phaseStruct);
+      end
+      
+
+      if phaseHandler.numPhases == 1
+        varsStruct = phaseStruct;
+      end
+      
       
     end
     
@@ -251,26 +276,60 @@ classdef Simultaneous < handle
       
     end
     
-    function [costs,constraints,constraints_LB,constraints_UB,times] = getNLPFun(self,nlpVars)
-  
+    
+    function [costs,constraints,constraints_LB,constraints_UB,times] = getNLP(self, nlpVars)
+      
+      lastPhaseNumStates = self.phaseHandler.get(self.numPhases).nx;
+      
+      xF = nlpVars(self.totalVars-lastPhaseNumStates:self.totalVars);
+      
+      costs = 0;
+      
+      constraints = cell(self.numPhases,1);
+      constraints_LB = cell(self.numPhases,1);
+      constraints_UB = cell(self.numPhases,1);
+      
+      varIndex = 1;
+      for k=1:self.numPhases
+        
+        phase = self.phaseHandler.get(k);
+        phaseVars(varIndex:varIndex+phase.numVars);
+        
+        [phaseCosts,phaseConstraints,phaseConstraints_LB,phaseConstraints_UB, times, x0, p0] = getPhaseEquations(phase,phaseVars);
+        [bc,bc_lb,bc_ub] = phase.ocpHandler.boundaryConditionsFun.evaluate(x0, xF, p0);
+        
+        constraints{k} = [phaseConstraints;bc];
+        constraints_LB{k} = [phaseConstraints_LB;bc_lb];
+        constraints_UB{k} = [phaseConstraints_UB;bc_ub];
+        
+        costs = costs + phaseCosts;
+        
+        varIndex = varIndex + phase.numVars;
+        xF = x0;
+      end
+      
+    end
+    
+    function [costs,constraints,constraints_LB,constraints_UB,times,x0,p0] = getPhaseEquations(phase,phaseVars)
+      
       % number of variables in one control interval
       % + 1 for the timestep
-      nci = self.nx+self.ni+self.nu+self.np+1;
+      nci = phase.nx+phase.ni+phase.nu+phase.np+1;
       
       % Finds indizes of the variables in the NlpVars array.
       % cellfun is similar to python list comprehension 
       % e.g. [range(start_i,start_i+nx) for start_i in range(1,nv,nci)]
-      X_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+self.nx-1)', 1:nci:self.nv, 'UniformOutput', false));
-      I_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+self.ni-1)', self.nx+1:nci:self.nv, 'UniformOutput', false));
-      U_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+self.nu-1)', self.nx+self.ni+1:nci:self.nv, 'UniformOutput', false));
-      P_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+self.np-1)', self.nx+self.ni+self.nu+1:nci:self.nv, 'UniformOutput', false));
-      H_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i)', self.nx+self.ni+self.nu+self.np+1:nci:self.nv, 'UniformOutput', false));
+      X_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+phase.nx-1)', 1:nci:phase.nv, 'UniformOutput', false));
+      I_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+phase.ni-1)', phase.nx+1:nci:phase.nv, 'UniformOutput', false));
+      U_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+phase.nu-1)', phase.nx+phase.ni+1:nci:phase.nv, 'UniformOutput', false));
+      P_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i+phase.np-1)', phase.nx+phase.ni+phase.nu+1:nci:phase.nv, 'UniformOutput', false));
+      H_indizes = cell2mat(arrayfun(@(start_i) (start_i:start_i)', phase.nx+phase.ni+phase.nu+phase.np+1:nci:phase.nv, 'UniformOutput', false));
       
-      X = reshape(nlpVars(X_indizes), self.nx, self.N+1);
-      I = reshape(nlpVars(I_indizes), self.ni, self.N);
-      U = reshape(nlpVars(U_indizes), self.nu, self.N);
-      P = reshape(nlpVars(P_indizes), self.np, self.N);
-      H = reshape(nlpVars(H_indizes), 1      , self.N);
+      X = reshape(phaseVars(X_indizes), phase.nx, phase.N+1);
+      I = reshape(phaseVars(I_indizes), phase.ni, phase.N);
+      U = reshape(phaseVars(U_indizes), phase.nu, phase.N);
+      P = reshape(phaseVars(P_indizes), phase.np, phase.N);
+      H = reshape(phaseVars(H_indizes), 1      , phase.N);
       
       % path constraints on first and last state
       pc0 = [];
@@ -279,66 +338,63 @@ classdef Simultaneous < handle
       pcf = [];
       pcf_lb = [];
       pcf_ub = [];
-      if self.options.path_constraints_at_boundary
-        [pc0, pc0_lb, pc0_ub] = self.ocpHandler.pathConstraintsFun.evaluate(X(:,1), P(:,1));
-        [pcf,pcf_lb,pcf_ub] = self.ocpHandler.pathConstraintsFun.evaluate(X(:,end), P(:,end));
+      if phase.options.path_constraints_at_boundary
+        [pc0, pc0_lb, pc0_ub] = phase.ocpHandler.pathConstraintsFun.evaluate(X(:,1), P(:,1));
+        [pcf,pcf_lb,pcf_ub] = phase.ocpHandler.pathConstraintsFun.evaluate(X(:,end), P(:,end));
       end         
       
       T0 = [0, cumsum(H(:,1:end-1))];
       
-      [xend_arr, ~, cost_arr, int_eq_arr, int_times] = self.integratorMap.evaluate(X(:,1:end-1), I, U, T0, H, P);
-      [pc_eq_arr, pc_lb_arr, pc_ub_arr] = self.pathconstraintsMap.evaluate(X(:,2:end-1), P(:,2:end));
+      [xend_arr, ~, cost_arr, int_eq_arr, int_times] = phase.integratorMap.evaluate(X(:,1:end-1), I, U, T0, H, P);
+      [pc_eq_arr, pc_lb_arr, pc_ub_arr] = phase.pathconstraintsMap.evaluate(X(:,2:end-1), P(:,2:end));
                 
       % timestep constraints
       h_eq = [];
       h_eq_lb = [];
       h_eq_ub = [];
       
-      if isempty(self.ocpHandler.T)
+      if isempty(phase.ocpHandler.T)
         % normalized timesteps (sum of timesteps is 1)
-        H_norm = self.ocpHandler.H_norm;
+        H_norm = phase.ocpHandler.H_norm;
         
         % h0 = h_1_hat / h_0_hat * h1 = h_2_hat / h_1_hat * h2 ...
         H_ratio = H_norm(1:end-1)./H_norm(2:end);
         h_eq = H_ratio .* H(:,2:end) - H(:,1:end-1);
-        h_eq_lb = zeros(1, self.N-1);
-        h_eq_ub = zeros(1, self.N-1);
+        h_eq_lb = zeros(1, phase.N-1);
+        h_eq_ub = zeros(1, phase.N-1);
       end
       
       % Parameter constraints 
       % p0=p1=p2=p3 ...
       p_eq = P(:,2:end)-P(:,1:end-1);
-      p_eq_lb = zeros(self.np, self.N-1);
-      p_eq_ub = zeros(self.np, self.N-1);
+      p_eq_lb = zeros(phase.np, phase.N-1);
+      p_eq_ub = zeros(phase.np, phase.N-1);
       
       % continuity (nx x N)
       continuity = xend_arr - X(:,2:end);
       
       % merge integrator equations, continuity, and path constraints,
       % timesteps constraints
-      shooting_eq    = [int_eq_arr(:,1:self.N-1);  continuity(:,1:self.N-1);  pc_eq_arr;  h_eq;     p_eq];
-      shooting_eq_lb = [zeros(self.ni,self.N-1);   zeros(self.nx,self.N-1);   pc_lb_arr;  h_eq_lb;  p_eq_lb];
-      shooting_eq_ub = [zeros(self.ni,self.N-1);   zeros(self.nx,self.N-1);   pc_ub_arr;  h_eq_ub;  p_eq_ub];
+      shooting_eq    = [int_eq_arr(:,1:phase.N-1);  continuity(:,1:phase.N-1);  pc_eq_arr;  h_eq;     p_eq];
+      shooting_eq_lb = [zeros(phase.ni,phase.N-1);   zeros(phase.nx,phase.N-1);   pc_lb_arr;  h_eq_lb;  p_eq_lb];
+      shooting_eq_ub = [zeros(phase.ni,phase.N-1);   zeros(phase.nx,phase.N-1);   pc_ub_arr;  h_eq_ub;  p_eq_ub];
       
       % reshape shooting equations to column vector, append final integrator and
       % continuity equations
-      shooting_eq    = [shooting_eq(:);    int_eq_arr(:,self.N); continuity(:,self.N)];
-      shooting_eq_lb = [shooting_eq_lb(:); zeros(self.ni,1);     zeros(self.nx,1)    ];
-      shooting_eq_ub = [shooting_eq_ub(:); zeros(self.ni,1);     zeros(self.nx,1)    ];
-      
-      % boundary constraints
-      [bc,bc_lb,bc_ub] = self.ocpHandler.boundaryConditionsFun.evaluate(X(:,1),X(:,end),P(:,1));
+      shooting_eq    = [shooting_eq(:);    int_eq_arr(:,phase.N); continuity(:,phase.N)];
+      shooting_eq_lb = [shooting_eq_lb(:); zeros(phase.ni,1);     zeros(phase.nx,1)    ];
+      shooting_eq_ub = [shooting_eq_ub(:); zeros(phase.ni,1);     zeros(phase.nx,1)    ];
       
       % collect all constraints
-      constraints = vertcat(pc0, shooting_eq, pcf, bc);
-      constraints_LB = vertcat(pc0_lb, shooting_eq_lb, pcf_lb, bc_lb);
-      constraints_UB = vertcat(pc0_ub, shooting_eq_ub, pcf_ub, bc_ub);
+      constraints = vertcat(pc0, shooting_eq, pcf);
+      constraints_LB = vertcat(pc0_lb, shooting_eq_lb, pcf_lb);
+      constraints_UB = vertcat(pc0_ub, shooting_eq_ub, pcf_ub);
       
       % terminal cost
-      costf = self.ocpHandler.arrivalCostsFun.evaluate(X(:,end),P(:,end));
+      costf = phase.ocpHandler.arrivalCostsFun.evaluate(X(:,end),P(:,end));
       
       % discrete cost
-      costD = self.ocpHandler.discreteCostsFun.evaluate(nlpVars); 
+      costD = phase.ocpHandler.discreteCostsFun.evaluate(phaseVars); 
       
       % sum all costs
       costs = sum(cost_arr) + costf + costD;
@@ -346,6 +402,9 @@ classdef Simultaneous < handle
       % times
       times = [T0; int_times; T0];
       times = [times(:); T0(end)+H(end)];
+      
+      x0 = X(:,1);
+      p0 = P(:,1);
       
     end % getNLPFun    
   end % methods
