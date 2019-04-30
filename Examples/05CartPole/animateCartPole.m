9156fe08de64e8c71e363be171d612819e6a343c
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-04-30 15:35:21 +0200

    cartpole video writer, animate path

--- Examples/05CartPole/animateCartPole.m
+++ Examples/05CartPole/animateCartPole.m
@@ -1,81 +1,106 @@
-function handles = animateCartPole(sol,times)
+function handles = animateCartPole(sol,times,recordVideo)
 
-  handles = {};
+  if nargin < 3
+    recordVideo = false;
+  end
+
+  if recordVideo
+    filename = ['Workspace/cartpole_', datestr(now,'yyyy-mm-dd_HHMMSS')];
+    v = VideoWriter(filename);
+    
+    T = times.states.value;
+    assert(all(T)==T(1));
+    v.FrameRate = floor(1./(times.states(2).value-times.states(1).value));
+    open(v);
+  end
+
   pmax = max(abs(sol.states.p.value));
+  [fig,handles] = create_fig(pmax);
   
-  states = sol.integrator.states.value;
-  times = times.integrator.value;
+  states = sol.states.value;
+  times = times.states.value;
   times = times(:);
   
   for k=2:length(times)
     t = times(k);
     x = states(:,k);
     dt = t-times(k-1);
-    handles = draw(t, dt, x, [0,0,0,0], pmax, handles);
+    draw(t, dt, x, handles);
+    
+    if recordVideo
+      frame = getframe(fig);
+      writeVideo(v, frame);
+    end
+  end
+  
+  if recordVideo
+    close(v)
   end
 
 end
 
-function handles = draw(time, dt, x, Xref, pmax, handles)
+
+function [fig,handles] = create_fig(pmax)
+  
+  fig = figure;
+  set(fig,'Color','white')
+  fig.OuterPosition = fig.InnerPosition;
+  xticks(-5:5)
+  yticks(-5:5)
+  pbaspect([16 9 1])
+  daspect([1 1 1])
+
+  hold on;
+  
+  ms = 10;
+
+  line([-pmax pmax], [0 0], 'color', 'k', 'Linewidth',1.5); hold on;
+  line([-pmax -pmax], [-0.1 0.1], 'color', 'k', 'Linewidth',1.5); hold on;
+  line([pmax pmax], [-0.1 0.1], 'color', 'k', 'Linewidth',1.5); hold on;
+
+  h2 = text(-0.3,pmax, '0.00 s','FontSize',15);
+  h3 = plot(0,0,'ks','MarkerSize',ms,'Linewidth',3);
+
+
+  h4 = line(0, 0,'color',[38,124,185]/255,'Linewidth',2);
+  h5 = plot(0,0,'o', 'color',[170,85,0]/255,'MarkerSize',ms,'Linewidth',3);
+
+  grid on;
+  ylim([-pmax pmax]);
+
+  handles = {h2,h3,h4,h5};
+
+  hold off;
+  pause(1)
+    
+end
+
+function draw(time, dt, x, handles)
   p = x(1);
   theta = x(2);
   t = time;
-
   l = 1.0;
-  ms = 10;
+  
+  [h2,h3,h4,h5] = handles{:};
 
-  if isempty(handles)
+  xB = p+l*sin(theta);
+  yB = l*cos(theta);
 
-    figure;
+  set(h2, 'String', sprintf('%.2f s', t));
 
-    hold on;
-    x_target = Xref(1);
-    y_target = 0;
+  set(h3, 'Xdata', p)
 
-    line([-pmax pmax], [0 0], 'color', 'k', 'Linewidth',1.5); hold on;
-    line([-pmax -pmax], [-0.1 0.1], 'color', 'k', 'Linewidth',1.5); hold on;
-    line([pmax pmax], [-0.1 0.1], 'color', 'k', 'Linewidth',1.5); hold on;
+  set(h4,'Xdata',[p xB]);
+  set(h4,'Ydata',[0 yB]);
 
-    plot(x_target, y_target, 'x', 'color', [38,124,185]/255, 'MarkerSize', ms, 'Linewidth', 2);
-    h2 = text(-0.3,pmax, '0.00 s','FontSize',15);
-    h3 = plot(p,0,'ks','MarkerSize',ms,'Linewidth',3);
-
-    xB = p-l*sin(theta);
-    yB = l*cos(theta);
-
-    h4 = line([p xB], [0 yB],'color',[38,124,185]/255,'Linewidth',2);
-    h5 = plot(xB,yB,'o', 'color',[170,85,0]/255,'MarkerSize',ms,'Linewidth',3);
-
-    grid on;
-    xlim([-pmax-l pmax+l]);
-    ylim([-pmax-l pmax+l]);
-
-    handles = {h2,h3,h4,h5};
-
-    hold off;
-    pause(1)
-
-  else
-    [h2,h3,h4,h5] = handles{:};
-
-    xB = p+l*sin(theta);
-    yB = l*cos(theta);
-
-    set(h2, 'String', sprintf('%.2f s', t));
-
-    set(h3, 'Xdata', p)
-
-    set(h4,'Xdata',[p xB]);
-    set(h4,'Ydata',[0 yB]);
-
-    set(h5,'Xdata',xB);
-    set(h5,'Ydata',yB); 
-  end
+  set(h5,'Xdata',xB);
+  set(h5,'Ydata',yB); 
 
   global testRun
   if isempty(testRun) || (testRun==false)
     pause(dt);
   end
+  
 end
 
 
