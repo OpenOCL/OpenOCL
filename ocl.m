843d36abadc88a54c2b3622ff39109187df17118
Jonas C K <jonas@be.gy>
2019-05-18 20:12:42 +0200

    Add files via upload

---
+++ ocl.m
@@ -0,0 +1,40 @@
+%% OpenOCL Open Optimal Control Library
+% Software for solving optimal control, trajectory optimization, and
+% model predictive control.
+%
+% Web: <a href="https://openocl.org">https://openocl.org/</a>
+%
+% API docs: <a href="https://openocl.org">https://openocl.org/api-docs/</a>
+%
+%% Get started
+%
+% Run an example with:
+%   
+%   ocl.examples.cartpole
+% 
+% Get a list of all examples:
+%
+%   help ocl.examples
+%
+% Look at example code:
+%
+%   open mainCartPole
+%
+%% List of packages
+% 
+% * ocl
+% * ocl.examples
+% 
+%% List of classes
+%
+% * ocl.System (OclSystem)
+% * ocl.OCP (OclOCP)
+% * ocl.Options (OclOptions)
+% * ocl.Solver (OclSolver)
+%
+%% List of functions
+%
+% * ocl.plot
+% * ocl.stairs
+%
+
