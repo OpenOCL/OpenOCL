387da4eba3e5f7a0d03458f8d874e99c6fe0d422
Jonas C K <Jonas.Koenemann@yahoo.de>
2019-05-18 20:17:04 +0200

    Update ocl.m

--- ocl.m
+++ ocl.m
@@ -38,6 +38,7 @@
 % * ocl.stairs
 %
 %% Copyright notice
+%
 % Copyright 2019 Jonas Koenemann, Moritz Diehl, University of Freiburg
 % Redistribution is permitted under the 3-Clause BSD License terms. Please
 % ensure the above copyright notice is visible in any derived work.
