cb07082a82182c5d34be6c7156961caee64a58c1
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-05-29 17:08:09 +0200

    bouncing ball initial end states

--- Examples/06BouncingBall/mainBouncingBall.m
+++ Examples/06BouncingBall/mainBouncingBall.m
@@ -1,18 +1,18 @@
 function [sol,times,ocl] = mainBouncingBall  
 
   options = OclOptions();
-  options.nlp.controlIntervals = 40;
-  options.nlp.collocationOrder = 2;
+  options.nlp.controlIntervals = 20;
+  options.nlp.collocationOrder = 3;
   
-  before_contact = OclPhase([], @before_contact_vars, @before_contact_ode);
+  before_contact = OclPhase(0.452, @before_contact_vars, @before_contact_ode);
   after_contact = OclPhase(0.452, @after_contact_vars, @after_contact_ode, @after_contact_cost);
 
   before_contact.setInitialStateBounds('s', 1);
   before_contact.setInitialStateBounds('v', 0);
-  before_contact.setEndStateBounds('s', 1);
+  before_contact.setEndStateBounds('s', 0);
   
   after_contact.setEndStateBounds('s', 1);
-  after_contact.setEndStateBounds('v', 1);
+  after_contact.setEndStateBounds('v', 0);
   
   ocl = OclSolver({before_contact, after_contact}, {@phase_transition}, options);
 
@@ -28,11 +28,11 @@ function [sol,times,ocl] = mainBouncingBall
   
   % phase 2
   figure; hold on; grid on;
-  oclStairs(times{2}.controls, sol{2}.controls.F)
+  oclStairs(times{2}.controls, sol{2}.controls.F*100)
   xlabel('time [s]');
-  oclPlot(times{1}.states, sol{1}.states.s)
+  oclPlot(times{2}.states, sol{2}.states.s)
   xlabel('time [s]');
-  oclPlot(times{1}.states, sol{1}.states.v)
+  oclPlot(times{2}.states, sol{2}.states.v)
   xlabel('time [s]');
   legend({'F','s','v'})
   xlabel('time [s]');
@@ -70,8 +70,10 @@ function after_contact_cost(self,k,K,x,~)
 end
 
 function phase_transition(ch, x0, xF)
+  % x0 current phase
+  % xF previous phase
   ch.add(x0.s, '==', xF.s);
-  ch.add(x0.v, '==', -xF.v / 2);
+  ch.add(x0.v, '==', -xF.v);
   ch.add(x0.iF, '==', 0);
 end
 
