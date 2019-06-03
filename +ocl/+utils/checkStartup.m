f311b8f445ecedb7874c43462d11596b56442fb8
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-06-03 15:40:06 +0200

    test check startup

--- +ocl/+utils/checkStartup.m
+++ +ocl/+utils/checkStartup.m
@@ -6,7 +6,7 @@ function checkStartup()
   persistent been_here
   if isempty(been_here) || ~been_here
     disp('Running OpenOCL setup procedure. This may required your input, and may take a while at the first time.')
-    StartupOCL
+    ocl.utils.StartupOCL();
     been_here = true;
   end
 end
