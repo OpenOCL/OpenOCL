3b3923eada2f90f322d744903dd656347b33defb
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-06-04 15:54:33 +0200

    doc list of examples

--- +ocl/examples.m
+++ +ocl/examples.m
@@ -1,16 +1,16 @@
 %% List of optimal control examples
 % 
-% * ocl.examples.vanderpol
-% * ocl.examples.ballandbeam
-% * ocl.examples.pendulum
-% * ocl.examples.racecar
-% * ocl.examples.cartpole
-% * ocl.examples.bouncingball (multi-stage)
+% * <a href="matlab:open ocl.examples.ballandbeam">ocl.examples.ballandbeam</a>
+% * <a href="matlab:open ocl.examples.bouncingball">ocl.examples.bouncingball</a> (multi-stage)
+% * <a href="matlab:open ocl.examples.cartpole">ocl.examples.cartpole</a>
+% * <a href="matlab:open ocl.examples.pendulum">ocl.examples.pendulum</a>
+% * <a href="matlab:open ocl.examples.racecar">ocl.examples.racecar</a>
+% * <a href="matlab:open ocl.examples.vanderpol">ocl.examples.vanderpol</a>
 %
 %% List of simulation examples
 %
-% * ocl.examples.pendulum_sim
-% * ocl.examples.bouncingball_sim (multi-stage)
+% * <a href="matlab:open ocl.examples.bouncingball">ocl.examples.bouncingball</a> (multi-stage)
+% * <a href="matlab:open ocl.examples.pendulum_sim">ocl.examples.pendulum_sim</a> 
 %
 function examples
   ocl.examples.cartpole
