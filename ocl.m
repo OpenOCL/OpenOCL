2c60fe5c92c48b1853cd9f6d5de14a7a779cbd3d
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-06-04 16:21:03 +0200

    ocl getting started two stage bouncing sim

--- ocl.m
+++ ocl.m
@@ -22,7 +22,7 @@
 %% List of optimal control examples
 % 
 % * <a href="matlab:open ocl.examples.ballandbeam">ocl.examples.ballandbeam</a>
-% * <a href="matlab:open ocl.examples.bouncingball">ocl.examples.bouncingball</a> (multi-stage)
+% * <a href="matlab:open ocl.examples.bouncingball">ocl.examples.bouncingball</a> (two-stage)
 % * <a href="matlab:open ocl.examples.cartpole">ocl.examples.cartpole</a>
 % * <a href="matlab:open ocl.examples.pendulum">ocl.examples.pendulum</a>
 % * <a href="matlab:open ocl.examples.racecar">ocl.examples.racecar</a>
@@ -30,7 +30,7 @@
 %
 %% List of simulation examples
 %
-% * <a href="matlab:open ocl.examples.bouncingball">ocl.examples.bouncingball</a> (multi-stage)
+% * <a href="matlab:open ocl.examples.bouncingball_sim">ocl.examples.bouncingball_sim</a> (multi-stage)
 % * <a href="matlab:open ocl.examples.pendulum_sim">ocl.examples.pendulum_sim</a> 
 %
 %% Copyright notice
