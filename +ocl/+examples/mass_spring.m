6821e54ff71ab2029e984c3f2ac6d2fe0665bc75
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-15 11:17:04 +0200

    mv mass spring

--- +ocl/+examples/mass_spring.m
+++ +ocl/+examples/mass_spring.m
@@ -1,9 +1,9 @@
 num_masses = 4;
 
-varsfh = @(h) ocl.examples.linear_mass_spring.vars(h,num_masses);
-daefh = @(h,x,z,u,p) ocl.examples.linear_mass_spring.dae(h,x,u);
-pathcostsfh = @(h,x,z,u,p) ocl.examples.linear_mass_spring.pathcosts(h,x,u);
-gridcostsfh = @(h,k,K,x,p) ocl.examples.linear_mass_spring.gridcosts(h,k,K,x);
+varsfh = @(h) ocl.examples.mass_spring.vars(h,num_masses);
+daefh = @(h,x,z,u,p) ocl.examples.mass_spring.dae(h,x,u);
+pathcostsfh = @(h,x,z,u,p) ocl.examples.mass_spring.pathcosts(h,x,u);
+gridcostsfh = @(h,k,K,x,p) ocl.examples.mass_spring.gridcosts(h,k,K,x);
 gridconstraintsfh = @(varargin) [];
 
 x0 = zeros(2*num_masses, 1);
