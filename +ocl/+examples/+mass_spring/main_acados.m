edf51be993e3b24d336f7275923620fc433a3c03
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-22 22:53:11 +0200

    real values acados bounds

--- +ocl/+examples/+mass_spring/main_acados.m
+++ +ocl/+examples/+mass_spring/main_acados.m
@@ -5,13 +5,13 @@ daefh = @(h,x,z,u,p) ocl.examples.mass_spring.dae(h,x,u);
 pathcostsfh = @(h,x,z,u,p) ocl.examples.mass_spring.pathcosts(h,x,u);
 gridcostsfh = @(h,k,K,x,p) ocl.examples.mass_spring.gridcosts(h,k,K,x);
 
-solver = ocl.acados.Solver(10, varsfh, daefh, gridcostsfh, pathcostsfh, 'N', 30);
+solver = ocl.acados.Solver(10, varsfh, daefh, pathcostsfh, gridcostsfh, 'N', 20);
 
 x0 = zeros(2*num_masses, 1);
 x0(1) = 2.5;
 x0(2) = 2.5;
 
-solver.setInitialState('x', x0);
+% solver.setInitialState('x', x0);
 
 % ig = solver.getInitialGuess();
 solver.solve();
