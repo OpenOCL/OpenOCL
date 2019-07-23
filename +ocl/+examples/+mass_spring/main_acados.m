28f6cdb3b0622967e25ee489083ac1163e608901
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-23 13:54:34 +0200

    mv AcadosSolver, return sol trajectories times

--- +ocl/+examples/+mass_spring/main_acados.m
+++ +ocl/+examples/+mass_spring/main_acados.m
@@ -5,7 +5,7 @@ daefh = @(h,x,z,u,p) ocl.examples.mass_spring.dae(h,x,u);
 pathcostsfh = @(h,x,z,u,p) ocl.examples.mass_spring.pathcosts(h,x,u);
 gridcostsfh = @(h,k,K,x,p) ocl.examples.mass_spring.gridcosts(h,k,K,x);
 
-solver = ocl.acados.Solver(10, varsfh, daefh, pathcostsfh, gridcostsfh, 'N', 20);
+solver = ocl.acados.AcadosSolver(10, varsfh, daefh, pathcostsfh, gridcostsfh, 'N', 20);
 
 x0 = zeros(2*num_masses, 1);
 x0(1) = 2.5;
@@ -14,24 +14,14 @@ x0(2) = 2.5;
 solver.setInitialState('x', x0);
 
 % ig = solver.getInitialGuess();
-[x,u] = solver.solve();
+[sol,times] = solver.solve();
 
 figure()
 subplot(2, 1, 1)
-plot(0:20, x);
+ocl.plot(times.states, sol.states.x);
 title('trajectory')
 ylabel('x')
 subplot(2, 1, 2)
-plot(1:N, u);
+ocl.stairs(times.controls, sol.controls.u.');
 ylabel('u')
 xlabel('sample')
-
-% figure()
-% subplot(2, 1, 1)
-% oclPlot(times.states, sol.states.x);
-% title('trajectory')
-% ylabel('x')
-% subplot(2, 1, 2)
-% oclStairs(times.controls, sol.controls.u.');
-% ylabel('u')
-% xlabel('sample')
