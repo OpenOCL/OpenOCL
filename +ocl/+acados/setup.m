7db9b6115b394be6a08d1e2865c6910b31d61244
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-23 20:02:18 +0200

    acados dir check

--- +ocl/+acados/setup.m
+++ +ocl/+acados/setup.m
@@ -8,7 +8,7 @@ rmpath(fullfile(oclPath, 'Lib', 'casadi'));
 acados_dir = getenv('ACADOS_INSTALL_DIR');
 
 if isempty(acados_dir)
-    oclError('Please run the acados_setup.sh script in the main directory of OpenOCL. Close Matlab and run it from the command prompt. Only Linux is supported with the acados interface.'); 
+    error('Please run the acados_setup.sh script in the main directory of OpenOCL. Close Matlab and run it from the command prompt. Only Linux is supported with the acados interface.'); 
 end
 
 addpath(fullfile(acados_dir, 'external', 'casadi-matlab'))
