477d828daa1d815648e74bfe4688cb57f80f0ef6
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-07-27 14:55:44 +0200

    mv acados Solver, eco4wind wind demo, time optimal

--- +ocl/+examples/+cartpole/main_eco4wind_demo.m
+++ +ocl/+examples/+cartpole/main_eco4wind_demo.m
@@ -1,52 +1,31 @@
 clear all;
 
-solver = ocl.Solver(3, ...
+ipopt_solver = ocl.Solver( ...
+  3, ...
   'vars', @ocl.examples.cartpole.vars, ...
   'dae', @ocl.examples.cartpole.dae, ...
   'pathcosts', @ocl.examples.cartpole.pathcosts, ...
   'N', 80, 'd', 3);
 
-p0 = 0; v0 = 0;
-theta0 = 180*pi/180; omega0 = 0;
+ipopt_solver.setInitialState('p', 0);
+ipopt_solver.setInitialState('v', 0);
+ipopt_solver.setInitialState('theta', pi);
+ipopt_solver.setInitialState('omega', 0);
 
-solver.setInitialState('p', p0);
-solver.setInitialState('v', v0);
-solver.setInitialState('theta', theta0);
-solver.setInitialState('omega', omega0);
-
-% solver.initialize('theta', [0 1], [pi 0]);
-
-% Run solver to obtain solution
-[sol,times] = solver.solve();
-
-% visualize solution
-figure; hold on; grid on;
-ocl.stairs(times.controls, sol.controls.F/10.)
-xlabel('time [s]');
-ocl.plot(times.states, sol.states.p)
-xlabel('time [s]');
-ocl.plot(times.states, sol.states.v)
-xlabel('time [s]');
-ocl.plot(times.states, sol.states.theta)
-legend({'force [10*N]','position [m]','velocity [m/s]','theta [rad]'})
-xlabel('time [s]');
-
-ocl.examples.cartpole.animate(sol,times);
-
-
-acados_solver = ocl.acados.AcadosSolver(3, ...
+acados_solver = ocl.acados.Solver( ...
+  3, ...
   'vars', @ocl.examples.cartpole.vars, ...
   'dae', @ocl.examples.cartpole.dae, ...
   'pathcosts', @ocl.examples.cartpole.pathcosts, ...
-  'N', 80, 'd', 3);
+  'N', 80);
 
-p0 = 0; v0 = 0;
-theta0 = 180*pi/180; omega0 = 0;
+acados_solver.setInitialState('p', 0);
+acados_solver.setInitialState('v', 0);
+acados_solver.setInitialState('theta', pi);
+acados_solver.setInitialState('omega', 0);
 
-acados_solver.setInitialState('p', p0);
-acados_solver.setInitialState('v', v0);
-acados_solver.setInitialState('theta', theta0);
-acados_solver.setInitialState('omega', omega0);
+%% Run first ipopt and then acados
+[sol,times] = ipopt_solver.solve();
 
 x_times_norm = times.states.value;
 x_times_norm = x_times_norm / max(x_times_norm);
