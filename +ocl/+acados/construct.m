f5cf9a02a217bb1a3c17c29f7cf8bbccae747e74
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-08-07 10:36:05 +0200

    compile mex at acados setup only once, #134

--- +ocl/+acados/construct.m
+++ +ocl/+acados/construct.m
@@ -100,7 +100,7 @@ sim_method_num_steps = 3;
 
 ocp_opts = acados_ocp_opts();
 nlp_solver = 'sqp';
-ocp_opts.set('compile_mex', 'true');
+ocp_opts.set('compile_mex', 'false');
 ocp_opts.set('codgen_model', 'true');
 ocp_opts.set('param_scheme', 'multiple_shooting_unif_grid');
 ocp_opts.set('param_scheme_N', N);
