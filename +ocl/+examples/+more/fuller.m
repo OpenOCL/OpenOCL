9864a7eeb1d37998395ef47e030ee3ffe55f4826
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-09-12 15:52:26 +0200

    exmaples mintoc fuller

--- +ocl/+examples/+more/fuller.m
+++ +ocl/+examples/+more/fuller.m
@@ -1,49 +1,59 @@
 % Problem formulation from
-%   BOCOP - A collection of examples
-%   Frederic Bonnans, Pierre MartinonD. Giorgi, V. Grelard, B. Heymann, J. Liu, S. Maindrault, O. Tissot
-%   https://files.inria.fr/bocop/Examples-BOCOP.pdf
+%   https://mintoc.de/index.php/Fuller%27s_problem
 %
 % TBV
 %
 function fuller
 
-  problem = ocl.Problem(3.5, ...
+  problem = ocl.Problem(1, ...
     @vars, ...
     @dynamics, ...
     @pathcosts, ...
-    'N', 200, 'd', 2);
+    'N', 50, 'd', 2);
   
-  problem.setInitialState('s', 0)
-  problem.setInitialState('sd', 1)
+  problem.setInitialState('x1', 0.01)
+  problem.setInitialState('x2', 0)
   
-  problem.setEndBounds('s', 0);
-  problem.setEndBounds('sd', 0);
+  problem.setEndBounds('x1', 0.01);
+  problem.setEndBounds('x2', 0);
   
   [sol, times] = problem.solve();
   
   figure;
+  subplot(3,1,1);
+  ocl.plot(times.states, sol.states.x1);
+  ylabel('x1')
+  xlim([0 1]);
+  subplot(3,1,2);
+  ocl.plot(times.states, sol.states.x2);
+  ylabel('x2')
+  xlim([0 1]);
+  subplot(3,1,3);
   ocl.stairs(times.controls, sol.controls);
+  ylabel('u')
+  xlim([0 1]);
+  xlabel('time')
 end
 
 
 function vars(vh)
 
-  vh.addState('s')
-  vh.addState('sd');
+  vh.addState('x1')
+  vh.addState('x2');
   
-  vh.addControl('u', 'lb', -1, 'ub', 1);
+  vh.addControl('u', 'lb', 0, 'ub', 1);
 
 end
 
 function dynamics(odeh, x, z, u, p)
 
-  odeh.setODE('s', x.sd);
-  odeh.setODE('sd', u);
+  odeh.setODE('x1', x.x2);
+  odeh.setODE('x2', 1-2*u);
 
 end
 
 function pathcosts(ch, x, z, u, p)
 
-  ch.add(x.s^2);
+  ch.add(x.x1^2);
 
 end
