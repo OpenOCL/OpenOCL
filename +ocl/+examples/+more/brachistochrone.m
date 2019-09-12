c9909777a82a847e527efc68f5260f9e096bc913
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2019-09-12 13:42:12 +0200

    brachistochrone example

---
+++ +ocl/+examples/+more/brachistochrone.m
@@ -0,0 +1,69 @@
+% Problem formulation from
+%   http://www.gpops2.com/Examples/Brachistochrone.html
+%
+% TBV
+%
+function brachistochrone
+
+  problem = ocl.Problem([], ...
+    @vars, ...
+    @dynamics, ...
+    'terminalcost', @terminalcost, ...
+    'N', 200, 'd', 3);
+  
+  problem.setInitialState('time', 0);
+  
+  problem.setInitialState('x', 0);
+  problem.setInitialState('y', 0);
+  problem.setInitialState('v', 0);
+  
+  problem.setEndBounds('x', 2);
+  problem.setEndBounds('y', 2);
+  
+  ig = problem.ig();
+  ig.controls.set(1);
+  
+  [sol, times] = problem.solve(ig);
+  
+  figure;
+  subplot(2,1,1);
+  ocl.plot(times.controls, sol.controls.u);
+  xlabel('time')
+  ylabel('slope')
+  subplot(2,1,2);
+  ocl.plot(sol.states.x, sol.states.y)
+  xlabel('x')
+  ylabel('y')
+
+end
+
+
+function vars(vh)
+
+  vh.addState('x');
+  vh.addState('y');
+  vh.addState('v');
+  
+  vh.addState('time');
+  
+  vh.addControl('u');
+
+end
+
+function dynamics(dh, x, z, u, p)
+
+  g = 9.81;
+
+  dh.setODE('x', x.v * sin(u));
+  dh.setODE('y', x.v * cos(u));
+  dh.setODE('v', g * cos(u));
+
+  dh.setODE('time', 1);
+  
+end
+
+function terminalcost(ch, x, p)
+
+  ch.add(x.time);
+
+end
