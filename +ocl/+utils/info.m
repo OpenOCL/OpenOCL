e4028f9206e177a7b53ce10ba917a393a50ace22
Jonas Koenemann <Jonas.Koenemann@yahoo.de>
2020-04-08 11:53:24 +0200

    define newline for Matlab<2016b

--- +ocl/+utils/info.m
+++ +ocl/+utils/info.m
@@ -17,7 +17,7 @@ for k=1:d
   spaces = strfind(msg_part, ' ');
   
   new_offset = N - spaces(end);
-  new_msg = sprintf('%s %s %s', new_msg, msg( (k-1)*N-offset+1 : k*N-new_offset), newline);
+  new_msg = sprintf('%s %s %s', new_msg, msg( (k-1)*N-offset+1 : k*N-new_offset), ocl.utils.newline);
   offset = new_offset;
 end
-fprintf('%s %s %s', new_msg, msg( d*N-offset+1 : end), newline);
+fprintf('%s %s %s', new_msg, msg( d*N-offset+1 : end), ocl.utils.newline);
